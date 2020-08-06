17585289c35f79dd7136875aff47329e54086a60
Артур <arthur.betsa@gmail.com>
2020-08-06 17:54:10 +0300

    херня непонятная с сортировкой

--- main.js
+++ main.js
@@ -1,72 +1,37 @@
+'use strict';
+
 let dataFromFile;
 let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let orderedData = {};
+let orderedDataString = {};
 
 
 fetch('words.txt')
     .then(response => response.text())
     .then(data => {
-        dataFromFile = data.split('\n');
+        dataFromFile = data.split('\n').sort();
+        dataFromFile = dataFromFile.sort((a,b)=>a-b?-1:1);
+
+
+        letterFinder(dataFromFile, alphabet);
+        fillDataObj();
+
+
     })
     .then(() => {
 
-        fillDataObj();
+
+
+        // console.log(orderedDataString);
+
         console.log(orderedData);
-
-        for (let key in orderedData) {
-            let b = orderedData[key].map((value, index) => {
-                return orderedData[key][index][0];
-            });
-        }
-;
-
+        console.log(dataFromFile[199634]);
     });
-
-function finder(arr, elem) {
-    if (!elem) {
-        return `Empty data!`;
-    }
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === elem) {
-            return renderAnswer(arr, i);
-        }
-    }
-    return `Not Found!`;
-}
-
-function renderAnswer(arr, index) {
-    result = `1: word "${arr[index]}" found in "${index}" row. Similar words: `;
-    for (let j = 1; j < 9; j++) {
-        if (arr[index + j]) {
-            result += `<br> ${j + 1}: word  "${arr[index + j]}" in "${index + j}"`;
-        }
-    }
-    return result;
-}
-
-window.addEventListener("load", () => {
-    document.getElementById("submit_data").addEventListener("click", onSubmit, false);
-
-});
-
-let onSubmit = function (event) {
-    event.preventDefault();
-    let inputValue = document.querySelector("#input").value.toString().trim();
-    let output_data = document.querySelector("#output>p");
-    output_data.innerHTML = finder(dataFromFile, inputValue);
-
-
-};
 
 
 function fillDataObj() {
-    let i = 0;
-    let others =[];
-    while (dataFromFile[i].toLowerCase() < "a"){
-        others.push(dataFromFile[i]);
-        i++;
-    }
-    orderedData["0"] = others;
+    orderedData[dataFromFile[0]] = dataFromFile.slice(0, 36);
+
     alphabet.map((value) => {
         orderedData[value] = arrDivider(divideByLetter(dataFromFile, value));
     })
@@ -83,15 +48,73 @@ function divideByLetter(undividedArr, letter) {
 };
 
 
-function arrDivider(arr) {
-    let dividedArr = [];
+function arrDivider(arrSortetByLetter) {
+    let dividedArr = {};
     let size = 1000; //размер подмассива
-    for (let i = 0; i < Math.ceil(arr.length / size); i++) {
-        dividedArr[i] = arr.slice((i * size), (i * size) + size);
+    for (let i = 0; i < Math.ceil(arrSortetByLetter.length / size); i++) {
+
+        dividedArr[i] = {
+            words: arrSortetByLetter.slice((i * size), (i * size) + size),
+            title: arrSortetByLetter[i * size],
+            indexOfTitle: dataFromFile.indexOf(arrSortetByLetter[i * size]),
+        };
     }
+
     return dividedArr;
 }
 
+//////////// input handler
+window.addEventListener("load", () => {
+    document.getElementById("submit_data").addEventListener("click", onSubmit, false);
+});
+
+let onSubmit = function (event) {
+    event.preventDefault();
+    let inputValue = document.querySelector("#input").value.trim().toString();
+    let output_data = document.querySelector("#output>p");
+    output_data.innerHTML = finder(dataFromFile, inputValue);
 
 
-//123123
+};
+
+function finder(arr, elem) {
+    if (!elem) {
+        return `Empty data!`;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === elem) {
+            return renderAnswer(arr, i);
+        }
+    }
+    return `Not Found!`;
+}
+
+
+function renderAnswer(arr, index) {
+    let result = `1: word "${arr[index]}" found in "${index}" row. Similar words: `;
+    for (let j = 1; j <= 9; j++) {
+        if (arr[index + j]) {
+            result += `<br> ${j + 1}: word  "${arr[index + j]}" in "${index + j}"`;
+        }
+    }
+    return result;
+}
+
+
+function letterFinder(dataArr, ruleArr) {
+    let i = 0;
+    let letter = 0;
+    while (i < dataArr.length) {
+
+        if (dataArr[i][0].toLowerCase() === ruleArr[letter]) {
+            orderedDataString[letter] = {
+                words: dataArr[i],
+                index: i,
+            };
+            letter++;
+        }
+
+        i++;
+    }
+}
+
